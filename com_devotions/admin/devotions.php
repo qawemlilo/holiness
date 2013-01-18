0d6b3f41f8da44e2909e955f8455a90f0214cf8f
Qawelesizwe <qawemlilo@gmail.com>
2013-01-18 15:29:48 +0200

    remved outdated com_devotions component

--- com_devotions/admin/devotions.php
+++
@@ -1,1 +0,0 @@
-<h1>Devotions</h1>
--- com_devotions/site/files/script.js
+++
@@ -1,8 +0,0 @@
-jQuery.noConflict();
-(function($) {
-    $(function() { 
-        $("#dt").datepicker({
-            dateFormat: 'DD, d MM, yy'
-        });      
-    });
-})(jQuery);
